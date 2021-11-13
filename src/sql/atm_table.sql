d36f928224e116926e3075cb7c5298bde8c677f8
e_casasanero <kkat1545@protonmail.com>
2021-11-12 17:04:39 -0800

    Added atm_table.sql

---
+++ src/sql/atm_table.sql
@@ -0,0 +1,6 @@
+create table ATM(ID int primary key auto_increment,
+					Address varchar(255),
+					Ones varchar(100),
+                    Fives varchar(100),
+                    Twenties varchar(100),
+                    Hundreds varchar(100)) Engine=InnoDB;
