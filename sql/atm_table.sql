1f72a82283d222b6174f055d1216a34638da31b5
voidstarr <voidstarr@users.noreply.github.com>
2021-11-29 15:33:20 -0800

    update table create statements

--- sql/atm_table.sql
+++ sql/atm_table.sql
@@ -1,6 +1,11 @@
-create table ATM(ID int primary key auto_increment,
-					Address varchar(255),
-					Ones varchar(100),
-                    Fives varchar(100),
-                    Twenties varchar(100),
-                    Hundreds varchar(100)) Engine=InnoDB;
+CREATE TABLE `ATM` (
+                       `ID` int NOT NULL AUTO_INCREMENT,
+                       `Address` varchar(255) DEFAULT NULL,
+                       `Ones` varchar(100) DEFAULT NULL,
+                       `Fives` varchar(100) DEFAULT NULL,
+                       `Tens` varchar(100) DEFAULT NULL,
+                       `Twenties` varchar(100) DEFAULT NULL,
+                       `Fifties` varchar(100) DEFAULT NULL,
+                       `Hundreds` varchar(100) DEFAULT NULL,
+                       PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
